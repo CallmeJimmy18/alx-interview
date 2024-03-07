9e522e4da9bfef095c7b9fdd716ddd7d0e9bc4a7
Gman <matolagarnet@gmail.com>
2024-03-07 13:27:10 +0000

    syntax fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,14 +3,15 @@
 
 
 def pascal_triangle(n):
-    triangle = []
-
     if n <= 0:
         return []
+
+    triangle = []
     for rows in range(n):
-        row = [None for i in range(rows + 1)]
+        row = [None] * (rows + 1)
         row[0], row[-1] = 1, 1
         for k in range(1, len(row) - 1):
+            # Calculate the middle elements of each row
             row[k] = triangle[rows - 1][k - 1] + triangle[rows - 1][k]
         triangle.append(row)
 
