03627bb5b4f749a014d905f660fc3e0c8bd191b4
Gman <matolagarnet@gmail.com>
2024-03-07 13:18:19 +0000

    Pascals triangle file complete

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This is a pythin algorithm of pascal's triangle """
+
+
+def pascal_triangle(n):
+    triangle = []
+    for rows in range(n):
+        row = [None for i in range(rows + 1)]
+        row[0], row[-1] = 1, 1
+        for k in range(1, len(row) - 1):
+            row[k] = triangle[rows - 1][k - 1] + triangle[rows - 1][k]
+        triangle.append(row)
+
+    return triangle
