3d55bfeba0cef8ed5b64e71c48b47e63973604d5
Gman <matolagarnet@gmail.com>
2024-03-28 15:08:15 +0000

    interview file now executable

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -24,7 +24,8 @@ if __name__ == "__main__":
     log = {}
     log["file_size"] = 0
     log["code_frequency"] = {
-        str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
     try:
         for line in sys.stdin:
