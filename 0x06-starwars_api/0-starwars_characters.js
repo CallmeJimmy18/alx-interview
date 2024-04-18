7d0bbb884b335f7525d05bcb73aa3a42b10c579d
Gman <matolagarnet@gmail.com>
2024-04-18 08:24:53 -0700

    semistandard style complient

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,29 +4,29 @@
 const request = require('request');
 
 function stwCharacters(filmId) {
-	const mainUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}/`
+  const mainUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}/`
 
-	request(mainUrl, (error, response, body) => {
-		if (error) {
-			console.error('Error:', error);
-		} else if (response.statusCode !== 200) {
-			console.error('Status:', response.statusCode);
-		} else {
-			const filmData = JSON.parse(body);
-			filmData.characters.forEach(charUrl => {
-				request(charUrl, (error, response, body) => {
-					if (error) {
-						console.error('Error:', error);
-					} else if (response.statusCode !== 200) {
-						console.error('Status:', response.statusCode);
-					} else {
-						const charData = JSON.parse(body);
-						console.log(charData.name);
-					}
-				});
-			});
-		}
-	});
+  request(mainUrl, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      } else if (response.statusCode !== 200) {
+        console.error('Status:', response.statusCode);
+      } else {
+        const filmData = JSON.parse(body);
+        filmData.characters.forEach(charUrl => {
+          request(charUrl, (error, response, body) => {
+            if (error) {
+              console.error('Error:', error);
+            } else if (response.statusCode !== 200) {
+              console.error('Status:', response.statusCode);
+            } else {
+              const charData = JSON.parse(body);
+              console.log(charData.name);
+            }
+          });
+        });
+      }
+  });
 }
 
 
