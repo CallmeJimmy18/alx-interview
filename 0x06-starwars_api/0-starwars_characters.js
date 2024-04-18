7f459f95d63e3ca4a50e8ccbdfc6df0ea3231525
Gman <matolagarnet@gmail.com>
2024-04-18 07:56:03 -0700

    Added project

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+
+const request = require('request');
+
+function stwCharacters(filmId) {
+	const mainUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}/`
+
+	request(mainUrl, (error, response, body) => {
+		if (error) {
+			console.error('Error:', error);
+		} else if (response.statusCode !== 200) {
+			console.error('Status:', response.statusCode);
+		} else {
+			const filmData = JSON.parse(body);
+			filmData.characters.forEach(charUrl => {
+				request(charUrl, (error, response, body) => {
+					if (error) {
+						console.error('Error:', error);
+					} else if (response.statusCode !== 200) {
+						console.error('Status:', response.statusCode);
+					} else {
+						const charData = JSON.parse(body);
+						console.log(charData.name);
+					}
+				});
+			});
+		}
+	});
+}
+
+
+const filmId = process.argv[2]
+if (filmId) {
+	stwCharacters(filmId);
+}
